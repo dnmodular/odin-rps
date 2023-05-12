bb612ae5268c52f392a4b419c029c90c022bf13d
dnmodular <dnmodular@gmail.com>
2023-05-12 12:51:42 -0400

    Edit rock-paper-scissors.js -Found that an array was a compact way to get computer's choice. -Touched up the game() function to run most of the game through it. -Added score values as global vars. -Included scoring inside the playRound() function.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,48 +3,41 @@ Donovan Narvaez
 This program will play 5 rounds of rock paper scissors.
 */
 
+let computerScore = 0;
+let playerScore = 0;
+
 // This function gets the computer's choice via rng.
 function getComputerChoice() {
-
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
-
-    const rock = "Rock";
-    const paper = "Paper";
-    const scissors = "Scissors";
-
-    let computerChoice = '';
-    if (randomNumber === 1) {
-        computerChoice = rock;
-    } else if (randomNumber === 2) {
-        computerChoice = paper;
-    } else if (randomNumber === 3) {
-        computerChoice = scissors;
-    }
+    let choices = ['Rock', 'Paper', 'Scissors'];
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
 }
 
 // This function plays a round of rps and returns a win/loss/draw string.
-function playRound(computerSelection) {
-    
-    let playerSelection = prompt('rock paper or scissors?');
-
+function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
 
     if (playerSelection === computerSelection) {
         return "Draw!";
     } else if (playerSelection !== computerSelection) {
         if ((playerSelection === "Rock") && (computerSelection === "Paper")) {
+            computerScore += 1;
             return "You lose! Paper beats Rock";
         } else if ((playerSelection === "Paper") && (computerSelection === "Scissors")) {
+            computerScore += 1;
             return "You lose! Scissors beats Paper";
         } else if ((playerSelection === "Scissors") && (computerSelection === "Rock")) {
+            computerScore += 1;
             return "You lose! Rock beats Scissors";
 
         } else if ((playerSelection === "Rock") && (computerSelection === "Scissors")) {
+            playerScore += 1;
             return "You win! Rock beats Scissors";
         } else if ((playerSelection === "Paper") && (computerSelection === "Rock")) {
+            playerScore += 1;
             return "You win! Paper beats Rock";
         } else if ((playerSelection === "Scissors") && (computerSelection === "Paper")) {
+            playerScore += 1;
             return "You win! Scissors beats Paper";
         }
     }
@@ -52,14 +45,23 @@ function playRound(computerSelection) {
 
 // This function brings all the other functions
 // together to play 5 rounds of rps and decides a winner.
-function game(x, y) {
-    for (let numRounds = 0; numRounds < 5; numRounds ++) {
-        playRound(x, y);
-       
+function game() {
+    let numRounds = 0;
+
+    while (numRounds < 5) {
+        const playerSelection = prompt('rock paper or scissors?');
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        numRounds ++;
     }
 
-    return 0;
+    if (computerScore > playerScore) {
+        return `Computer wins! ${computerScore} to ${playerScore}!`;
+    } else {
+        return `Player wins! ${playerScore} to ${computerScore}!`;
+    }
 }
 
-const computerSelection = getComputerChoice();
-console.log(game(playerSelection, computerSelection));
+
+
+console.log(game());
